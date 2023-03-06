76e829916c476c5db3f9e8b75d0440fed52ed0d9
Irfnmdlptra <bangasrat@gmail.com>
2023-03-06 11:18:53 +0800

    Todolist complate

---
+++ src/Container/Pages/Home/Forms/Button.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../../../Styles/Form/Button.css";
+
+
+class Button extends Component {
+  render() {
+    return (
+        <button>
+          Button
+        </button>
+    )
+  }
+}
+
+export default Button;
